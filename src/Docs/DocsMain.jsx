791ff4f45e64ba0f49b84b60ec552f2390c7241d
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-08-14 08:51:49 +0530

    React Router installed to write the docs

---
+++ src/Docs/DocsMain.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DocsMain() {
+  return (
+    <div className='w-full h-screen bg-gray-900 flex items-center justify-center'>
+      <h1>Welcome To Docs</h1>
+    </div>
+  )
+}
+
+export default DocsMain
