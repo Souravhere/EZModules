4a36c097f1aa642b01e1810d806b7d09add6ec48
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-08-15 19:46:28 +0530

    Few ui bugs fixed in indexData.jsx

--- src/MainComp/IndexData.jsx
+++ src/MainComp/IndexData.jsx
@@ -13,7 +13,9 @@ function IndexData() {
             {/* <p className="w-full text-center mb-2"><span className='text-orange-600 text-center bg-orange-300/20 px-2 py-1 font-semibold rounded-lg mx-auto'>Beta Release</span></p> */}
             <h1 className="font-semibold text-3xl text-center">Welcome To EZ Modules</h1>
             <p className='sm:w-1/2 text-center mx-auto text-sm my-3 font-semibold p-2' >EZModules is a versatile and open-source React component library, offering reusable UI and feature components for developers seeking to enhance their web projects efficiently.</p>
+            <div className='sm:w-1/2 w-64 sm:px-6 px-0 mx-auto'>
             <CodeHighlighter language="bash" code={installezm} />
+            </div>
             <Link to='/docs'>
             <p className='w-full text-center mb-2'>
             <Tooltip content="Go To Docs Section" position="top" trigger="hover" theme='dark'>
